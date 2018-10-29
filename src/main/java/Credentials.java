27e16e0dd4d521599aee4f5b3e63b4105e8755ea
woojiahao <woojiahao1234@gmail.com>
2018-10-29 17:01:15 +0800

    Completed the reset feature for the login screen

--- src/main/java/Credentials.java
+++ src/main/java/Credentials.java
@@ -1,10 +1,44 @@
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Labeled;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Credentials {
 	@FXML private TextField clientId;
 	@FXML private TextField clientSecret;
 	@FXML private Button start;
 	@FXML private Button reset;
+	@FXML private GridPane scopes;
+
+	@FXML public void initialize() {
+		reset.setOnMouseClicked(ev -> {
+			clientId.clear();
+			clientSecret.clear();
+			checkAllScopes();
+		});
+	}
+
+	private void checkAllScopes() {
+		scopes.getChildren()
+			.stream()
+			.filter(n -> n instanceof CheckBox)
+			.map(n -> (CheckBox) n)
+			.forEach(cb -> cb.setSelected(true));
+	}
+
+	private List<String> getCheckedScopes() {
+		return scopes.getChildren()
+			.stream()
+			.filter(n -> n instanceof CheckBox)
+			.map(n -> (CheckBox) n)
+			.filter(CheckBox::isSelected)
+			.map(Labeled::getText)
+			.collect(Collectors.toList());
+	}
 }
