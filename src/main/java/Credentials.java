af612c03ee016b8290bf2719c939f9006468ef60
woojiahao <woojiahao1234@gmail.com>
2018-10-29 17:16:29 +0800

    Completed the generation steps to get an authorization url

--- src/main/java/Credentials.java
+++ src/main/java/Credentials.java
@@ -1,27 +1,65 @@
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Labeled;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import me.chill.authentication.SpotifyAuthenticationHelper;
+import me.chill.authentication.SpotifyAuthorizationFlow;
+import me.chill.authentication.SpotifyScope;
 
+import javax.swing.*;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Credentials {
 	@FXML private TextField clientId;
 	@FXML private TextField clientSecret;
+	@FXML private TextField redirectUrl;
 	@FXML private Button start;
 	@FXML private Button reset;
 	@FXML private GridPane scopes;
 
 	@FXML public void initialize() {
-		reset.setOnMouseClicked(ev -> {
-			clientId.clear();
-			clientSecret.clear();
-			checkAllScopes();
-		});
+		reset.setOnMouseClicked(e -> resetDefaults());
+		start.setOnMouseClicked(e -> authorizeUser());
+	}
+
+	private void authorizeUser() {
+		String id = clientId.getText().trim();
+		String secret = clientSecret.getText().trim();
+		String url = redirectUrl.getText().trim();
+
+		if (id.isEmpty() || secret.isEmpty() || url.isEmpty()) {
+			JOptionPane.showMessageDialog(null, "Please fill in the client id, client secret and redirect url");
+			return;
+		}
+
+		List<String> selectedScopes = getSelectedScopes();
+		List<SpotifyScope> scopes = Arrays
+			.stream(SpotifyScope.values())
+			.filter(scope -> selectedScopes.contains(scope.getScopeName()))
+			.collect(Collectors.toList());
+
+		SpotifyAuthenticationHelper helper = new SpotifyAuthenticationHelper.Builder()
+			.setClientId(id)
+			.setClientSecret(secret)
+			.setRedirectUrl(url)
+			.setShowDialog(true)
+			.setScopes(scopes)
+			.build();
+
+		SpotifyAuthorizationFlow flow = new SpotifyAuthorizationFlow(helper);
+
+		String authorizationUrl = flow.generateAuthorizationUrl().toString();
+	}
+
+	private void resetDefaults() {
+		clientId.clear();
+		clientSecret.clear();
+		redirectUrl.clear();
+		checkAllScopes();
 	}
 
 	private void checkAllScopes() {
@@ -32,7 +70,7 @@ public class Credentials {
 			.forEach(cb -> cb.setSelected(true));
 	}
 
-	private List<String> getCheckedScopes() {
+	private List<String> getSelectedScopes() {
 		return scopes.getChildren()
 			.stream()
 			.filter(n -> n instanceof CheckBox)
