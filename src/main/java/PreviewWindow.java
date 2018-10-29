a7d964d9b557475c7985c8867901fb70cca9da5c
woojiahao <woojiahao1234@gmail.com>
2018-10-29 21:55:18 +0800

    completed the notification of refresh in preview window

--- src/main/java/PreviewWindow.java
+++ src/main/java/PreviewWindow.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.bcel.internal.generic.NEW;
+import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Platform;
@@ -25,26 +27,26 @@ public class PreviewWindow {
 		Platform.runLater(this::loadDetails);
 
 		clipboard.setOnMouseClicked(e -> copyToClipboard());
-		exit.setOnMouseClicked(e -> exitGenerator());
+		exit.setOnMouseClicked(e -> {
+			final int selection = JOptionPane.showConfirmDialog(null, "Are you sure you wish to exit?");
+			if (selection == JOptionPane.YES_OPTION) exitGenerator();
+		});
 	}
 
 	private void exitGenerator() {
-		final int selection = JOptionPane.showConfirmDialog(null, "Are you sure you wish to exit?");
-		if (selection == JOptionPane.YES_OPTION) {
-			user.disableTimer();
-			Stage stage = (Stage) clipboard.getScene().getWindow();
-			stage.close();
-		}
+		user.disableTimer();
+		Stage stage = (Stage) clipboard.getScene().getWindow();
+		stage.close();
 	}
 
 	private void copyToClipboard() {
 		copyText();
-		editLabel();
+		editLabel("Copied to clipboard", 3);
 	}
 
-	private void editLabel() {
-		title.setText("Access Token (Copied to clipboard):");
-		final Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(2), e -> title.setText("Access Token:")));
+	private void editLabel(String addOn, int duration) {
+		title.setText(String.format("Access Token (%s):", addOn));
+		final Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(duration), e -> title.setText("Access Token:")));
 		timeline.play();
 	}
 
@@ -56,9 +58,20 @@ public class PreviewWindow {
 	}
 
 	private void loadDetails() {
-		user.startTimer();
 		accessToken.setText(user.getAccessToken());
-
+		if (user.getExpiryDuration() == null) {
+			System.out.println("Unable to refresh tokens due to some unknown error with the expiry duration");
+			return;
+		}
+		final Timeline timeline = new Timeline(
+			new KeyFrame(Duration.seconds(user.getExpiryDuration() + 1),
+			e -> {
+				JOptionPane.showMessageDialog(null, "Access Token was refreshed");
+				editLabel("Refreshed", 5);
+				accessToken.setText(user.getAccessToken());
+			}));
+		timeline.setCycleCount(Animation.INDEFINITE);
+		timeline.play();
 	}
 
 	void setUser(SpotifyUser user) {
