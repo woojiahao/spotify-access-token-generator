335c7554f71af685b3e46f7446de93e391b65336
woojiahao <woojiahao1234@gmail.com>
2018-10-29 16:48:26 +0800

    Completed the basic login screen

---
+++ src/main/java/Credentials.java
@@ -0,0 +1,10 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Credentials {
+	@FXML private TextField clientId;
+	@FXML private TextField clientSecret;
+	@FXML private Button start;
+	@FXML private Button reset;
+}
