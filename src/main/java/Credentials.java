ba17ea128abd5ea4c2a5eb5206e695f4b8eb32ec
woojiahao <woojiahao1234@gmail.com>
2018-11-01 21:54:12 +0800

    added the ability to load from different configuration files by file selector

--- src/main/java/Credentials.java
+++ src/main/java/Credentials.java
@@ -4,17 +4,19 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Labeled;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import me.chill.authentication.SpotifyAuthenticationHelper;
 import me.chill.authentication.SpotifyAuthorizationFlow;
 import me.chill.authentication.SpotifyScope;
 
 import javax.swing.*;
+import java.awt.*;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -30,11 +32,12 @@ public final class Credentials {
 	@FXML private Button start;
 	@FXML private Button reset;
 	@FXML private GridPane scopes;
+	@FXML private MenuItem loadConfiguration;
 
 	private List<CheckBox> scopeOptions = new ArrayList<>();
 
 	@FXML public void initialize() {
-		loadInformation();
+		loadInformation(new File("config/config.json"));
 		scopeOptions.addAll(scopes
 			.getChildren()
 			.stream()
@@ -44,6 +47,16 @@ public final class Credentials {
 
 		reset.setOnMouseClicked(e -> resetFields());
 		start.setOnMouseClicked(e -> authorizeUser());
+		loadConfiguration.setOnAction(e -> loadConfigurationFromFile());
+	}
+
+	private void loadConfigurationFromFile() {
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Open Configuration File");
+		fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Configuration Files (*.json)", "*.json"));
+		File file = fileChooser.showOpenDialog(start.getScene().getWindow());
+
+		if (file != null) loadInformation(file);
 	}
 
 	private void authorizeUser() {
@@ -95,11 +108,10 @@ public final class Credentials {
 			.collect(Collectors.toList());
 	}
 
-	private void loadInformation() {
-		File config = new File("config/config.json");
-		if (config.exists()) {
+	private void loadInformation(File file) {
+		if (file.exists()) {
 			try {
-				JsonObject configDetails = new Gson().fromJson(new FileReader(config), JsonObject.class);
+				JsonObject configDetails = new Gson().fromJson(new FileReader(file), JsonObject.class);
 				if (!configDetails.has("id") || !configDetails.has("url") || !configDetails.has("secret")) {
 					System.out.println("Missing fields in config.json, ensure you have id, secret and url");
 					return;
